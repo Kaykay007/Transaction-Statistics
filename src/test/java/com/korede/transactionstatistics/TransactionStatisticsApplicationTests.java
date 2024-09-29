5a2adbdbe4d8e1cb9bae4d3404d1360ca70870c1
hameedakorede <hameed.korede.hk@gmail.com>
2024-09-30 00:11:02 +0100

    first commit

---
+++ src/test/java/com/korede/transactionstatistics/TransactionStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korede.transactionstatistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
