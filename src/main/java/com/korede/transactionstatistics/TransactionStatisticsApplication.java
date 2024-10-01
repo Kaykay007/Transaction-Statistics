9ad68b1f2ef362b9d9500198ff32bd4285992855
hameedakorede <hameed.korede.hk@gmail.com>
2024-10-01 22:40:32 +0100

    remove algo test in main class

--- src/main/java/com/korede/transactionstatistics/TransactionStatisticsApplication.java
+++ src/main/java/com/korede/transactionstatistics/TransactionStatisticsApplication.java
@@ -10,19 +10,9 @@ import java.util.List;
 public class TransactionStatisticsApplication {
 
     public static void main(String[] args) {
-        AlgorithmSolutions algo = new AlgorithmSolutions();
-        SpringApplication.run(TransactionStatisticsApplication.class, args);
-        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        List<int[]> mergedIntervals = algo.mergeIntervals(intervals);
-        System.out.println("Merged Intervals:");
-        for (int[] interval : mergedIntervals) {
-            System.out.println("[" + interval[0] + ", " + interval[1] + "]");
-        }
 
-        // Test maxSubarrayXOR
-        int[] arr = {1, 2, 3, 4};
-        int maxXor = algo.maxSubarrayXOR(arr);
-        System.out.println("Maximum XOR of subarray: " + maxXor);
+        SpringApplication.run(TransactionStatisticsApplication.class, args);
+
     }
 
 
